d18b48417acb9b027e294ce6b77845e1e146db92
shubham-manohara <shubham.manohara@bitontree.com>
2021-12-15 14:56:44 +0530

    added firstTimeUser component

---
+++ src/containers/FirstTimeUser/FirstTimeUser.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Button from '../../components/Button';
+import TextItem from '../../components/TextItem';
+import UserItem from '../../components/UserItem';
+
+FirstTimeUser.prototype = {
+  setSelectedUser: PropTypes.func.isRequired,
+};
+
+const contactList = [
+  {
+    id: 1,
+    title: 'Amanda Nano',
+    description: 'Hey there! I’m using Jur chat',
+  },
+  {
+    id: 2,
+    title: 'Rama Jr',
+    description: 'Busy',
+  },
+  {
+    id: 3,
+    title: 'George Silva',
+    description: 'Call if urgent',
+  },
+  {
+    id: 4,
+    title: 'Fu Shang',
+    description: 'New day, new challenges',
+  },
+  {
+    id: 5,
+    title: 'Person Name',
+    description: 'Chat only',
+  },
+  {
+    id: 6,
+    title: 'Person Name',
+    description: 'Hey there! I’m using Jur chat',
+  },
+];
+
+function FirstTimeUser({ setSelectedUser }) {
+  const [selectedContactId, setSelectedContactId] = useState(0);
+  return (
+    <div>
+      <div className="flex flex-col items-center mt-20">
+        <TextItem
+          text="Let us know who you are"
+          className="pt-4 text-5xl leading-10 font-normal"
+        />
+        <ul className="mt-20">
+          {contactList.map((contact) => (
+            <li key={contact.id}>
+              <UserItem
+                onClick={() => setSelectedContactId(contact.id)}
+                isSelected={selectedContactId === contact.id}
+                className="mt-5"
+                title={contact.title}
+                description={contact.description}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+      {!!selectedContactId && (
+        <Button
+          onClick={() => setSelectedUser(selectedContactId)}
+          className="absolute right-0 mt-8 md:p-5 mr-80"
+          shouldShowBorder
+          shouldShowShadow
+        >
+          Continue
+        </Button>
+      )}
+    </div>
+  );
+}
+
+export default FirstTimeUser;
